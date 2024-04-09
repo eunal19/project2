62d31d0b77e3d58036cf2e0c5a48fdfcd78a3cd9
emrah unal <actioninstem@gmail.com>
2024-04-09 13:04:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Timer() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [count]);
+
+  return <h1>I have rendered {count} times </h1>;
 }
 
-export default App;
+export default Timer;
